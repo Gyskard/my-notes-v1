37e7841c9139825cb20b8f6f08e930c540c7605f
Thomas Margueritat <thomas@marguerit.at>
2020-09-17 20:15:51 +0200

    added unit test for checkIfNotDesynchronisation function

--- src/back/test/mocha/app.js
+++ src/back/test/mocha/app.js
@@ -2,10 +2,19 @@ const assert = require('assert');
 const app = require('../../app.js');
 
 describe('app test', function() {
-    it('not found desynchronisation', function () {
-        assert.equal(app.checkIfNotDesynchronisation('./test/content/good_sync/data', './test/content/good_sync/manager.json'), true);
+    it('not found desynchronisation with one note', function () {
+        assert.strictEqual(app.checkIfNotDesynchronisation('./test/content/good_sync_1/data', './test/content/good_sync_1/manager.json'), true);
     });
-    it('found desynchronisation 1', function () {
-        assert.equal(app.checkIfNotDesynchronisation('../content/bad_sync_1/data', '../content/bad_sync_1/manager.json'), false);
+
+    it('not found desynchronisation with two notes', function () {
+        assert.strictEqual(app.checkIfNotDesynchronisation('./test/content/good_sync_2/data', './test/content/good_sync_2/manager.json'), true);
+    });
+
+    it('found desynchronisation with one note but incorret manager file', function () {
+        assert.strictEqual(app.checkIfNotDesynchronisation('../content/bad_sync_1/data', '../content/bad_sync_1/manager.json'), false);
+    });
+    
+    it('found desynchronisation with two notes but missing one note file', function () {
+        assert.strictEqual(app.checkIfNotDesynchronisation('../content/bad_sync_2/data', '../content/bad_sync_2/manager.json'), false);
     });
 });
