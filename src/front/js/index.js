6a7ab3cb6a0a2aecaed095b17acf5e9fc3a71da3
Thomas Margueritat <thomas@marguerit.at>
2020-10-12 21:23:50 +0200

    improved router system, doesn't work

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,15 +1,45 @@
 const Note = {
-  template: '<div>note {{ $route.params.id }}</note>'
+  template: `
+    <div>
+      <h2>Note {{ $route.params.id }}</h2>
+      <router-view></router-view>
+    </div>
+  `
 }
 
-const routes = [
-  { path: '/user/:id', component: Note }
-]
+const NoteList = {
+  template: `
+    <h3>List</h3>
+    <router-view></router-view>
+  `,
+  data () {
+    return {
+      list: null,
+      error: null
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    axios
+      .get('http://localhost:3000/list')
+      .then(reponse => (this.list = reponse))
+    console.log(this.list)
+  }
+}
+
+const NoteShow = { template: '<div>show</div>' }
+const NoteEdit = { template: '<div>edit</div>' }
 
 const router = new VueRouter({
-  routes
+  routes : [
+    {
+      path: '/list', component: NoteList,
+      path: '/note/:id', component: Note,
+      children: [
+        { path: 'show', component: NoteShow },
+        { path: 'edit', component: NoteEdit }
+      ]
+    }
+  ]
 })
 
-const app = new Vue({
-  router
-}).$mount('#app')
+const app = new Vue({ router }).$mount('#app')
