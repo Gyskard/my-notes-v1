b85d444c1a76caa1741a7926563ed4e3dbd061dd
Thomas Margueritat <thomas@marguerit.at>
2020-11-02 22:19:14 +0100

    improved css

--- src/front/my-notes/src/App.vue
+++ src/front/my-notes/src/App.vue
@@ -1,18 +1,31 @@
 <template>
-  <div id="app">
-    <h1>List</h1>
-    <NoteList />
-  </div>
+  <v-app>
+    <div id="app">
+      <v-card color="grey lighten-4" flat tile>
+        <v-toolbar>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          <v-toolbar-title>My notes</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </v-card>
+      <v-content>
+        <NoteList />
+      </v-content>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import NoteList from './components/NoteList.vue';
+import NoteList from "./components/NoteList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     NoteList,
-  }
+  },
 };
 </script>
