cd5f387325ff59421e39f43d2f64080eac9a8abe
Thomas Margueritat <thomas@marguerit.at>
2020-12-24 21:19:10 +0100

    added navigation drawers

--- src/front/my-notes/src/App.vue
+++ src/front/my-notes/src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <v-app>
-    <v-card color="grey lighten-4" flat tile>
-      <v-toolbar>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title> My notes </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-      </v-toolbar>
-    </v-card>
-    <v-main class="pt-10">
-      <router-view></router-view>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My notes</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <v-list nav dense>
+        <v-list-item-group active-class="text--accent-4">
+          <v-list-item :to="{ path: '/' }">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          <v-list-item :to="{ path: '/add' }">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Add a note</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+        <router-view class="pt-10"></router-view>
     </v-main>
   </v-app>
 </template>
+
+<style>
+  html {
+    overflow: hidden
+  }
+</style>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    })
+  }
+</script>
